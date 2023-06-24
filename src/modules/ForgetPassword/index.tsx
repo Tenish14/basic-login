4b8168762f72ec961e1e4b063d83b0d05c069af5
Tenish14 <Tenish14@gmail.com>
2023-06-25 01:25:57 +0800

    Make reset password send email

--- src/modules/ForgetPassword/index.tsx
+++ src/modules/ForgetPassword/index.tsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 export default function ForgetPassword(): JSX.Element {
+  const navigate = useNavigate();
   const [emailId, setEmailId] = useState<string>("");
+
+  const auth = getAuth();
+
+  const forgetPasswordOnClick: React.MouseEventHandler<
+    HTMLButtonElement
+  > = async (e) => {
+    e.preventDefault();
+    try {
+      await sendPasswordResetEmail(auth, emailId);
+      navigate("/sign-in");
+    } catch (error: any) {
+      console.log("Reset password", error.message);
+    }
+  };
 
   return (
     <div className="container">
@@ -24,12 +40,18 @@ export default function ForgetPassword(): JSX.Element {
                 />
               </Form.Group>
             </Form>
-            <Button variant="warning" className="my-3 mx-2">
+            <Button
+              variant="warning"
+              className="my-3 mx-2"
+              onClick={forgetPasswordOnClick}
+            >
               Reset password
             </Button>
             <div className="d-flex">
               <p>Back to login</p>
-              <Link to="/sign-in" className="mx-2">Login</Link>
+              <Link to="/sign-in" className="mx-2">
+                Login
+              </Link>
             </div>
           </div>
         </div>
